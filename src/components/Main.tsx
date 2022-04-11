e2ea0b5fec7db1f8bae2e66d75c9a9583b75670e
PromiSe#### <vicimpa@ya.ru>
2022-04-11 12:15:23 +0300

    Init project

---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import logoSrc from "logo.svg";
+
+export const Main = () => {
+  return (
+    <>
+      <h1>Hello react!</h1>
+      <h3>This is test app</h3>
+      <img src={logoSrc} />
+    </>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { Main } from "components/Main";
+import { StrictMode } from "react";
+import { render } from "react-dom";
+
+render(
+  <StrictMode>
+    <Main />
+  </StrictMode>,
+  document.getElementById('root')
+);
